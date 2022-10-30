41afa292c80d5f1a24b2cb3e9c1d9dc22fbada35
Guilherme Piccoli <gcopiccoli@outlook.com>
2022-10-30 11:19:34 +0000

    adjust footer styling - active class

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,49 +3,88 @@ import homeIcon from "../../assets/icons/home-icon.svg";
 import portfolioIcon from "../../assets/icons/wallet-icon.svg";
 import aboutIcon from "../../assets/icons/globe-icon.svg";
 import moreIcon from "../../assets/icons/more-icon.svg";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
+
   return (
     <footer className="footer">
       <section className="footer__container">
-        <article className="footer__wrapper">
-          <img
-            src={homeIcon}
-            alt="Home icon"
-            className="footer__icon footer__icon-home"
-          />
-          <p className="footer__icon-title">Home</p>
+        <article
+          className={
+            splitLocation[1] === "home"
+              ? "footer__wrapper--active"
+              : "footer__wrapper"
+          }
+        >
+          <Link className="footer__link" to="/home">
+            <img
+              src={homeIcon}
+              alt="Home icon"
+              className="footer__icon footer__icon-home"
+            />
+            <p className="footer__icon-title">Home</p>
+          </Link>
         </article>
-        <article className="footer__wrapper">
-          <img
-            src={portfolioIcon}
-            alt="Wallet icon"
-            className="footer__icon footer__icon-portfolio"
-          />
-          <p className="footer__icon-title">Portfolio</p>
+        <article
+          className={
+            splitLocation[1] === "portfolio"
+              ? "footer__wrapper--active"
+              : "footer__wrapper"
+          }
+        >
+          <Link className="footer__link" to="/portfolio/:id">
+            <img
+              src={portfolioIcon}
+              alt="Wallet icon"
+              className="footer__icon footer__icon-portfolio"
+            />
+            <p className="footer__icon-title">Portfolio</p>
+          </Link>
         </article>
         <article className="footer__wrapper-dca">
-          <p className="footer__icon-title">
-            <span className="footer__logo-bold">DCA</span>
-            <br />
-            <span className="footer__logo-italic">now</span>
-          </p>
+          <Link className="footer__link" to="/dca/:id">
+            <p className="footer__icon-title">
+              <span className="footer__logo-bold">DCA</span>
+              <br />
+              <span className="footer__logo-italic">now</span>
+            </p>
+          </Link>
         </article>
-        <article className="footer__wrapper">
-          <img
-            src={aboutIcon}
-            alt="About icon"
-            className="footer__icon footer__icon-about"
-          />
-          <p className="footer__icon-title">About</p>
+        <article
+          className={
+            splitLocation[1] === "about"
+              ? "footer__wrapper--active"
+              : "footer__wrapper"
+          }
+        >
+          <Link className="footer__link" to="/about">
+            <img
+              src={aboutIcon}
+              alt="About icon"
+              className="footer__icon footer__icon-about"
+            />
+            <p className="footer__icon-title">About</p>
+          </Link>
         </article>
-        <article className="footer__wrapper">
-          <img
-            src={moreIcon}
-            alt="More icon"
-            className="footer__icon footer__icon-more"
-          />
-          <p className="footer__icon-title">More</p>
+        <article
+          className={
+            splitLocation[1] === "more"
+              ? "footer__wrapper--active"
+              : "footer__wrapper"
+          }
+        >
+          <Link className="footer__link" to="/more">
+            <img
+              src={moreIcon}
+              alt="More icon"
+              className="footer__icon footer__icon-more"
+            />
+            <p className="footer__icon-title">More</p>
+          </Link>
         </article>
       </section>
     </footer>
