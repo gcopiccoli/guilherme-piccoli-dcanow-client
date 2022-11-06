817a77e1b5f25fd912af93fb7c8aa66d986cf399
Guilherme Piccoli <gcopiccoli@outlook.com>
2022-11-06 00:43:37 +0000

    add homepage and styling

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { UserAuth } from "../../context/AuthContext";
 import "./HomePage.scss";
 
@@ -7,8 +8,34 @@ const HomePage = () => {
   return (
     <section className="home">
       <article className="home__container">
-        <h1 className="home__title">Welcome, {user?.displayName}</h1>
+        <h1 className="home__title">
+          Welcome,{" "}
+          <span className="home__title-highlight">{user?.displayName}</span>
+        </h1>
+        <div className="home__feature">
+          <div className="home__image-one"></div>
+          <h3 className="home__copy-one">
+            DCAnow allows you to stay organised and
+          </h3>
+        </div>
+        <div className="home__feature">
+          <h3 className="home__copy-two">
+            on top of your high quality investments for your DCA strategy,
+          </h3>
+          <div className="home__image-two"></div>
+        </div>
+        <div className="home__feature">
+          <div className="home__image-three"></div>
+          <h3 className="home__copy-three">
+            having a clear view of your regular investments.
+          </h3>
+        </div>
       </article>
+      <div className="landing__button-box">
+        <Link to="/sigin">
+          <div className="landing__button">Build your portfolio</div>
+        </Link>
+      </div>
     </section>
   );
 };
